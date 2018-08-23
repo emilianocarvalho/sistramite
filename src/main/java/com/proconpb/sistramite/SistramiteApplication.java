d6b49039b8af44cad68a017ace295f2b679fdca4
Halyne Ribeiro <halyne.dangelo@gmail.com>
2018-08-23 15:01:32 -0300

    Projeto criado

---
+++ src/main/java/com/proconpb/sistramite/SistramiteApplication.java
@@ -0,0 +1,12 @@
+package com.proconpb.sistramite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistramiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistramiteApplication.class, args);
+	}
+}
