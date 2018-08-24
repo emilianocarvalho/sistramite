3a0702f48ae19ea7e1e416acc49b9a9ade269740
Halyne Ribeiro <halyne.dangelo@gmail.com>
2018-08-24 14:42:47 -0300

    Criando Operação de instanciação

--- src/main/java/com/proconpb/sistramite/SistramiteApplication.java
+++ src/main/java/com/proconpb/sistramite/SistramiteApplication.java
@@ -1,12 +1,33 @@
 package com.proconpb.sistramite;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SistramiteApplication {
+import com.proconpb.sistramite.domain.Setor;
+import com.proconpb.sistramite.repositories.SetorRepository;
 
+@SpringBootApplication
+public class SistramiteApplication implements CommandLineRunner{
+
+	@Autowired
+	private SetorRepository setorRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SistramiteApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Setor s1 = new Setor(1, "TI", 1);
+		Setor s2 = new Setor(2, "RH", 2);
+		
+		setorRepository.saveAll(Arrays.asList(s1, s2));
+		
+		
 	}
 }
