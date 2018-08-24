4376af30654546a1f857d333ab338181813062d5
Halyne Ribeiro <halyne.dangelo@gmail.com>
2018-08-24 09:38:45 -0300

    Banco de dados H2 e a criação automatica da base de dados

--- src/main/java/com/proconpb/sistramite/resources/SetorResource.java
+++ src/main/java/com/proconpb/sistramite/resources/SetorResource.java
@@ -1,15 +1,28 @@
 package com.proconpb.sistramite.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.proconpb.sistramite.domain.Setor;
 
 @RestController
 @RequestMapping(value="/setor")
 public class SetorResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Setor> listar() {
+		
+		Setor s1 = new Setor(1, "TI", 1);
+		Setor s2 = new Setor(2, "RH", 2);
+		
+		List<Setor> lista = new ArrayList<>();
+		lista.add(s1);
+		lista.add(s2);
+		
+		return lista;
 	}
 }
