705eba74a49f979059c218afebf4e35c44b0418e
Halyne Ribeiro <halyne.dangelo@gmail.com>
2018-10-01 14:43:10 -0300

    Permitindo adicionar uma nova Pessoa durante inserção de Auto/POST

--- src/main/java/com/proconpb/sistramite/services/AutoService.java
+++ src/main/java/com/proconpb/sistramite/services/AutoService.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.proconpb.sistramite.domain.Auto;
 import com.proconpb.sistramite.domain.PessoaFisica;
 import com.proconpb.sistramite.domain.PessoaJuridica;
 import com.proconpb.sistramite.repositories.AutoRepository;
+import com.proconpb.sistramite.repositories.PessoaFisicaRepository;
+import com.proconpb.sistramite.repositories.PessoaJuridicaRepository;
 import com.proconpb.sistramite.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,20 +21,27 @@ public class AutoService {
 	@Autowired
 	private AutoRepository repo;
 		
+	@Autowired
+	private PessoaFisicaRepository pFRepo;
+	
+	@Autowired
+	private PessoaJuridicaRepository pJRepo;
+	
 	public Auto find(Integer id) {
 		Optional<Auto> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Auto não encontrado! Id: " + id + ", Tipo: " + Auto.class.getName()));
 	}
 	
+	@Transactional
 	public Auto insert(Auto obj) {
 		obj.setId(null);		
 		if (obj.getEmpresa() instanceof PessoaJuridica){
 			PessoaJuridica empresa = (PessoaJuridica) obj.getEmpresa();
-			obj.setEmpresa(empresa);
+			pJRepo.save(empresa);
 		}		
 		if (obj.getFiscal() instanceof PessoaFisica){
 			PessoaFisica fiscal = (PessoaFisica) obj.getFiscal();
-			obj.setFiscal(fiscal);
+			pFRepo.save(fiscal);
 		}		
 		return repo.save(obj);
 	}
